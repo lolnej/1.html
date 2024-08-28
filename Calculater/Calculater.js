8fb7930bcf7b83baa953068dea369213ef71c1c7
lolnej <nicklas.bogetoft@gmail.com>
2024-08-28 09:38:08 +0200

    lommeregner
    Min lommeregner

---
+++ Calculater/Calculater.js
@@ -0,0 +1,34 @@
+function beregn() {
+    const tal1 = parseFloat(document.getElementById("tal1").value);
+    const regnetegn = document.getElementById("regnetegn").value;
+    const tal2 = parseFloat(document.getElementById("tal2").value);
+
+    let resultat;
+    switch (regnetegn) {
+      case "+":
+        resultat = tal1 + tal2;
+        break;
+      case "-":
+        resultat = tal1 - tal2;
+        break;
+      case "*":
+        resultat = tal1 * tal2;
+        break;
+      case "/":
+          resultat = tal1 / tal2;
+        if(tal2==0){
+          resultat = "At du prøver at dele med den mængde bitches du laver";
+        }
+        break;
+        case "^":
+       resultat=Math.pow(tal1,tal2);
+       break;
+       case "rt":
+        resultat=Math.pow(tal1,1/tal2);
+        break;
+      default:
+        resultat = "Ugyldigt regnetegn";
+    }
+
+    document.getElementById("resultat").innerHTML = "Resultat: " + resultat;
+  }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-let resImg1 = document.getElementById("research1");
-let p1 = document.querySelector(".para1");
 
-let sum = 0;
-let ar1 = [];
+let prog = 1;
+let ymer = 2;
+let rettap = 3;
+let lol = 4;
+let åse = 5;
 
-let obj1 = {};
+let obj1 = { name: "John", age: 210 };
+let obj2 = { city: "New York", country: "Denmark" };
+let obj3 = { Nicklas: "Programmer", experience: 0 };
+
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = ["John", "New York", "Programmer"];
+
+for (let i = 0; i < 5; i++) {
+   console.log("værdien af i er: " + i);
+}
 
